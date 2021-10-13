cc4c12b40fceaa81cb13a93156e422554d506e84
iemdevx <watcharapong_sribun@outlook.com>
2021-10-13 11:01:51 +0700

    add shape and layout

---
+++ lib/color_constant.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorConstatnt {
+  static const Color grayBG = Color(0xFFD8D8D8);
+
+  static const Color purple = Color(0xFFD27AFF);
+  static const Color blue = Color(0xFF90E5FF);
+  static const Color pink = Color(0xFFF9DFFF);
+  static const Color border = Color(0xFF707070);
+}
