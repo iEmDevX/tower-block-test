78d4c60511125405dc5a93ae092c3f406692876b
iemdevx <watcharapong_sribun@outlook.com>
2021-10-15 11:36:26 +0700

    Add dialog WG Test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,36 +1,17 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:tower_block_test/main.dart';
-import 'package:tower_block_test/shape/widget/circle_shape_wg.dart';
+import 'package:tower_block_test/widget/project/dialog.dart';
 
 void main() {
-  // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-  //   // Build our app and trigger a frame.
-  //   await tester.pumpWidget(const MyApp());
+  testWidgets('test dialog total time', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: DialogWG(timeUse: 12),
+      ),
+    );
 
-  //   // Verify that our counter starts at 0.
-  //   expect(find.text('0'), findsOneWidget);
-  //   expect(find.text('1'), findsNothing);
-
-  //   // Tap the '+' icon and trigger a frame.
-  //   await tester.tap(find.byIcon(Icons.add));
-  //   await tester.pump();
-
-  //   // Verify that our counter has incremented.
-  //   expect(find.text('0'), findsNothing);
-  //   expect(find.text('1'), findsOneWidget);
-  // });
-
-  testWidgets('Check Number Block', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
-    expect(find.byElementType(CircleShapeWG), findsOneWidget);
+    expect(find.text('TOTAL TIME'), findsOneWidget);
+    expect(find.text('12 S'), findsOneWidget);
+    expect(find.text('PLAY AGAIN'), findsOneWidget);
   });
 }
